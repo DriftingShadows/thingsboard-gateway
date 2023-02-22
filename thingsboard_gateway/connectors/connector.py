5e00aa845931514cbcba5737c6a015a076aab5a4
Ike Bai <ike@symbiot.technology>
2023-02-22 12:30:41 +1000

    config setup

--- thingsboard_gateway/connectors/connector.py
+++ thingsboard_gateway/connectors/connector.py
@@ -14,6 +14,7 @@
 
 import logging
 from abc import ABC, abstractmethod
+
 from thingsboard_gateway.gateway.constants import DEFAULT_SEND_ON_CHANGE_INFINITE_TTL_VALUE, DEFAULT_SEND_ON_CHANGE_VALUE
 
 log = logging.getLogger("connector")
